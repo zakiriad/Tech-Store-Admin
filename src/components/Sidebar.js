00a7ce16e0edf11b03595bb8abc3af5b74b0621f
Riad Zakaria Mordi <43653256+zakiriad@users.noreply.github.com>
2021-09-15 16:38:31 +0200

    modif

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,9 @@ import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "./SidebarData";
 import SubMenu from "./SubMenu";
 import { IconContext } from "react-icons/lib";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../actions/authActions";
+
 
 const Nav = styled.div`
 background: #15171c;
@@ -40,6 +43,9 @@ z-index: 10;
 const SidebarWrap = styled.div`
 width: 100%;
 `;
+const dispatch = useDispatch();
+const isAuthenticated = useSelector((state)=>state.auth.isAuthenticated);
+const Lougout = dispatch(logout);
 
 const Sidebar = () => {
 const [sidebar, setSidebar] = useState(false);
@@ -53,13 +59,34 @@ return (
 		<NavIcon to="#">
 			<FaIcons.FaBars onClick={showSidebar} />
 		</NavIcon>
-		<h1
-			style={{ textAlign: "center",
-					marginLeft: "200px",
-					color: "green" }}
-		>
-			GeeksforGeeks
-		</h1>
+		<div className="box2">
+			<img src="Logop.png"  alt=""/> 
+		</div>
+		
+		
+		<div className="box3">
+			<input type="text" placeholder="search" className="search"></input>  
+			<button className="search">search</button>
+		</div>
+		
+		
+		<div className="box4">
+			<IoIcons.IoMdNotifications className="icon-nav"/>
+		</div>
+		
+		
+		<div className="box5">
+			<Link to="/Profile" >
+				<FaIcons.FaUserCircle className="icon-nav"/>
+			</Link>
+		</div>
+		
+
+		<div className="box7">
+			<Button className="Logout" onClick={Logout}>
+				Logout
+			</Button> 
+		</div>
 		</Nav>
 		<SidebarNav sidebar={sidebar}>
 		<SidebarWrap>
